4dbb6044c8e110ed82e138e0adaaa074c6c841a9
yostan <wujunray@163.com>
2015-08-15 23:23:34 +0800

    add image support.

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,12 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
 
 
 class BlogPage(Page):
+    main_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+++')
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
@@ -17,5 +19,6 @@ class BlogPage(Page):
                                           index.SearchField('body'),)
 
     content_panels = Page.content_panels + [FieldPanel('date'),
+                                            ImageChooserPanel('main_image'),
                                             FieldPanel('intro'),
                                             FieldPanel('body', classname='full')]
